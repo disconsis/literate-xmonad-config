d1c6edf4347284e37a48b51e94ec6923a59736d2
disconsis <kk4168@nyu.edu>
2020-04-23 13:50:26 -0400

    add polybar-start-monitor script to repo

---
+++ bin/polybar-start-monitor.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <monitor>"
+fi
+
+# the single quotes are neccessary to prevent command substitution
+trap 'kill $(jobs -p)' SIGTERM EXIT
+
+monitor="$1"
+display=$(echo "$DISPLAY" | tr -d ':')
+fifo="/tmp/polybar-${display}.${monitor}-stdin.fifo"
+[ -p "$fifo" ] || mkfifo "$fifo"
+
+MONITOR="$monitor" STDINFIFO="$fifo" polybar xmonad-stdin &
+
+# wait for polybar reader to start
+while ! pgrep -axf "cat ${fifo}"; do : ; done
+
+cat > "$fifo"
