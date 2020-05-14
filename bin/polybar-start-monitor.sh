f6501d98032ae99064aad20294a004c1f02f78df
disconsis <kk4168@nyu.edu>
2020-05-14 17:53:56 -0400

    stop polybar kill messages on test end

--- bin/polybar-start-monitor.sh
+++ bin/polybar-start-monitor.sh
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]; then
 fi
 
 # the single quotes are neccessary to prevent command substitution
-trap 'kill $(jobs -p)' SIGTERM EXIT
+trap 'kill $(jobs -p) 2>/dev/null' SIGTERM EXIT
 
 monitor="$1"
 display=$(echo "$DISPLAY" | tr -d ':')
